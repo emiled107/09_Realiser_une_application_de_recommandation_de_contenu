38e008dc3b8b6416d0e36167b232164e10254847
Cyrille Guillaud <cyrilleguillaud@gmail.com>
2024-07-31 10:01:09 +0200

    maj upload

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/recommend', methods=['GET'])
-def recommend():
-    user_id = request.args.get('user_id')
-    response = requests.get(f"<URL_AZURE_FUNCTION>?user_id={user_id}")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get-recommendations', methods=['POST'])
+def get_recommendations():
+    user_id = request.form['user_id']
+    # URL de la fonction Azure déployée
+    azure_function_url = 'https://your-azure-function-url.azurewebsites.net/api/GetRecommendations'
+    # Appel à la fonction Azure avec l'ID utilisateur
+    response = requests.post(azure_function_url, json={'user_id': user_id})
     recommendations = response.json()
     return jsonify(recommendations)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
